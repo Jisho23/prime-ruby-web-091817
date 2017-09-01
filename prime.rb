b1b3e348fb7738ecaa2cd993e8a4c35e9e45141e
Joshua Denenberg <j.denenberg42@gmail.com>
2017-09-01 16:47:37 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(integer)
+  if integer <1
+    return false
+  elsif integer == 1
+    return false
+  end
+
+  counter = 2
+  while counter < integer
+    if integer % counter == 0
+      return false
+    end
+    counter += 1
+  end
+  return true
+end
